030f17e6dd8e325d9e4849cb21fe4066edb20d9d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-19 17:10:39 -0500

    Import blogs from database, map through blogs and create a BlogCard for each

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getBlogs } from '../../../services/blogs';
+import BlogCard from '../../BlogCard/BlogCard';
 
 export default function Main() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getBlogs();
+      setBlogs(data);
+    }
+    fetchData();
+  }, []);
   return (
-    <main>Main</main>
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </main>
   );
 }
